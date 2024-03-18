9c15595c811ec8848ca394215d0d3998007596b9
SUN <siksik2782259@gmail.com>
2024-03-18 18:18:17 +0900

    상속

---
+++ src/sec05/ch05/ex02/ShutDownButton.java
@@ -0,0 +1,19 @@
+package sec05.ch05.ex02;
+
+public class ShutDownButton  extends Button{
+    public ShutDownButton() {
+        super("ShutDown"); // 💡 부모의 생성자 호출
+    }
+
+    @Override
+    /*
+    `@Override` 어노테이션
+        - 어노테이션 : 이후 배울 것
+        - 부모의 특정 메소드를 오버라이드함을 명시
+        - 없어도 오류가 나지는 않음
+        - 붙였는데 메소드명이 다를 시 오류 (실수 방지)
+     */
+    public void func() {
+        System.out.printf("프로그램 종료");
+    }
+}
